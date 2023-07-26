d337e8d92583c52a04f5dac0a3f2b8c43bc09141
Maurice <mwasmorris777@gmail.com>
2023-07-26 19:53:34 +0300

    Added all prrequisites for creating the questions app

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# quiz_app
+This React app lets users answer questions and provides their final score after last question
---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex bg-slate-200 justify-center">
+      <h1>I'm back y'all!</h1>
+    </div>
+  );
+}
+
+export default App;
